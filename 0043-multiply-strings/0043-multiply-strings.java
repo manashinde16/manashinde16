667a3c49abcd898145f6226388b8e8e0a41730db
Manas Shinde <139111725+manashinde16@users.noreply.github.com>
2025-08-26 22:45:45 +0530

    Time: 3 ms (55.36%), Space: 42.2 MB (78.9%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String multiply(String num1, String num2) {
+    String multiply(String num1, String num2) {
         if (num1.equals("0") || num2.equals("0")) return "0";
 
         int m = num1.length();
