81090a125cc3154a9ab3214fc062a4427019318f
Manas Shinde <139111725+manashinde16@users.noreply.github.com>
2025-09-02 23:20:27 +0530

    Time: 102 ms (52.6%), Space: 70.6 MB (47.81%) - LeetHub

---
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -0,0 +1,28 @@
+class NumMatrix {
+    private int[][] prefix;
+
+    public NumMatrix(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) return;
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        prefix = new int[m + 1][n + 1];
+
+        // Build prefix sum
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                prefix[i + 1][j + 1] = matrix[i][j] 
+                                     + prefix[i][j + 1] 
+                                     + prefix[i + 1][j] 
+                                     - prefix[i][j];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return prefix[row2 + 1][col2 + 1] 
+             - prefix[row1][col2 + 1] 
+             - prefix[row2 + 1][col1] 
+             + prefix[row1][col1];
+    }
+}
